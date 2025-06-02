4fba2fd6e81375439a9f1070b50d3777d7681d54
Azizbek Zaripov <azizbekzaripov003@gmail.com>
2025-06-02 13:33:14 +0500

    dddd

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+import Data from './Data'
+
+const Dashboard = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Data/>
+        <Footer/>g
+    </div>
+  )
+}
+
+export default Dashboard
