5729442a27ca36bdf8d6885829edd2636c84baa1
Azizbek Zaripov <azizbekzaripov003@gmail.com>
2025-06-02 15:58:19 +0500

    dddd

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import photo1 from '../assets/photo-1.jpg'
 const Data = () => {
   return (
     <div>
@@ -10,7 +10,7 @@ const Data = () => {
     <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
         
         <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="/docs/images/carousel/carousel-1.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+            <img src={photo1} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
         </div>
         
         <div className="hidden duration-700 ease-in-out" data-carousel-item>
